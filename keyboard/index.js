c7a30d16223adf88d4a7e0fe5658adffb0d0da4c
pudelgulasch <katrinsalac@gmail.com>
2019-10-23 15:46:39 +0200

    add

--- keyboard/index.js
+++ keyboard/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("./keyboard.txt", "utf8", (data, err) => {
+fs.readFile("./keyboard.txt", "utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
 });
