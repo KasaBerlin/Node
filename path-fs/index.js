2d6ac3263de4b644e1eba094022b3e16d6ce364e
pudelgulasch <katrinsalac@gmail.com>
2019-10-23 14:50:37 +0200

    add

--- path-fs/index.js
+++ path-fs/index.js
@@ -130,3 +130,17 @@ fs.readFile("./data.json", "utf8", (err, data) => {
 //     console.log("File has been updated");
 //   });
 // });
+
+var data = {};
+data.table = [];
+for (i = 0; i < 26; i++) {
+  var obj = {
+    id: i,
+    square: i * i
+  };
+  data.table.push(obj);
+}
+fs.writeFile("input.json", JSON.stringify(data), function(err) {
+  if (err) throw err;
+  console.log("complete");
+});
