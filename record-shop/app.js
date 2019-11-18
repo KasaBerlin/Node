2d0cab83cb4e8b7ef4bd2d8cd53cc885917751b1
pudelgulasch <katrinsalac@gmail.com>
2019-11-18 11:09:28 +0100

    add

--- record-shop/app.js
+++ record-shop/app.js
@@ -1,46 +1,44 @@
 /** EXTERNAL DEPENDENCIES */
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
 /** ROUTERS */
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const recordsRouter = require('./routes/records');
-const ordersRouter = require('./routes/orders');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const recordsRouter = require("./routes/records");
+const ordersRouter = require("./routes/orders");
 const { setCors } = require("./middleware/security");
 
 /** INIT */
 const app = express();
 
 /** LOGGING */
-app.use(logger('dev'));
-
+app.use(logger("dev"));
 
 /** SETTING UP LOWDB */
-const adapter = new FileSync('data/db.json');
+const adapter = new FileSync("data/db.json");
 const db = low(adapter);
 db.defaults({
-    records: [],
-    users: [],
-    orders: []
+  records: [],
+  users: [],
+  orders: []
 }).write();
-
 
 /** SETTING UP MONGOOSE IN SERVER */
 const mongoose = require("mongoose");
+let faker = require("faker");
 mongoose.connect("mongodb://127.0.0.1:27017/recordShop", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
 });
 
-const dbRecords = mongoose.connection;
-dbRecords.on("error", console.log);
-dbRecords.on("open", () => {
+mongoose.connection.on("error", console.log);
+mongoose.connection.on("open", () => {
   console.log("Database connection is established...");
 });
 
@@ -51,30 +49,28 @@ app.use(cookieParser());
 app.use(setCors);
 
 /** STATIC FILES*/
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 /** ROUTES */
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/records', recordsRouter);
-app.use('/orders', ordersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/records", recordsRouter);
+app.use("/orders", ordersRouter);
 
 /** ERROR HANDLING */
-app.use(function (req, res, next) {
-    const error = new Error('Looks like something broke...');
-    error.status = 400;
-    next(error);
+app.use(function(req, res, next) {
+  const error = new Error("Looks like something broke...");
+  error.status = 400;
+  next(error);
 });
 
-app.use(function (err, req, res, next) {
-    res.send({
-        error: {
-            message: err.message
-        }
-    });
+app.use(function(err, req, res, next) {
+  res.send({
+    error: {
+      message: err.message
+    }
+  });
 });
-
 
 /** EXPORT PATH */
 module.exports = app;
